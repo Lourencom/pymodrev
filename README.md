51f53ef2a4c200a82c587be1c9bb77e63076c203
Lourenco <lourencodematos@gmail.com>
2023-03-23 13:16:45 +0000

    added conda recipe, modrev_setup

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Python interface to ModRev
 
 
-This Python module is an interface to the software tool ModRev (https://filipegouveia.github.io/ModelRevisionASP/) for model revision for Boolean logical models of biological regulatory networks.
+This Python module is an interface to the software tool ModRev (https://filipegouveia.github.io/ModelRevisionASP/) for model revision of Boolean logical models of biological regulatory networks.
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+NAME = 'modrev'
+
+setup(name=NAME,
+    version='9999',
+    author = "Lourenco Matos",
+    author_email = "lourencodematos@gmail.com",
+    url = "https://github.com/Lourencom/modrev-python",
+    description = "Python interface to ModRev",
+    long_description = """Provides interface to ModRev""",
+    install_requires = [
+        "py4j",
+        "colomoto_jupyter >= 0.6.3",
+    ],
+    py_modules = ["modrev_setup"],
+    packages = find_packages()
+)
