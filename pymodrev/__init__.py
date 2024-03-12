302cf58abeda5e9a28902d7619b5dcfb13ef5054
Lourenço Matos <lourenco.matos@tecnico.ulisboa.pt>
2024-03-12 20:55:18 +0000

    refactored some functions

--- pymodrev/__init__.py
+++ pymodrev/__init__.py
@@ -4,12 +4,6 @@ from colomoto_jupyter.sessionfiles import new_output_file
 
 from ginsim.gateway import japi
 
-
-def save(model, filename, format=None):
-    assert japi.lqm.save(model, filename, format)
-    return filename
-
-
 def reduce_to_prime_implicants(lqm):
     # japi.java is gateway
     if not isinstance(lqm, JavaObject):
@@ -36,122 +30,120 @@ def reduce_to_prime_implicants(lqm):
     return new_lqm
 
 
+def save(model, format=None):
+    filename = new_output_file(format)
+    assert japi.lqm.save(model, filename, format)
+    return filename
+
+
 class ModRev:
     modrev_path = "/home/lourenco/research/colomoto/ModRev/src/modrev"
 
     def __init__(self, lqm):
         self.lqm = lqm  # bioLQM model JavaObject
-
         self.prime_impl = reduce_to_prime_implicants(lqm)
+        self._save_model_to_modrev_file()
+        self.dirty_flag = False
+        self.observations = {}
+        self.repairs = {}
 
-        # file with lqm model in modrev format
+    def _save_model_to_modrev_file(self):
+        """
+        Saves the current model to a file in modrev format
+        """
         # fixme: format "lp" must come in the bioLQM dependency in colomoto-docker/jupyter
-        modrev_file = new_output_file("lp")
-        self.modrev_file = save(self.lqm, modrev_file, "lp")
+        self.modrev_file = save(self.lqm, "lp")
+        self.dirty_flag = False
 
-        self.dirty_flag = False  # bool to indicate if the object has been modified
-        self.obs_dict = {}  # dict to store observations
-
-    def add_obs(self, *args, **kwargs):
+    def _run_modrev(self, *args):
         """
-        Adds observations to the model. This function can handle a single observation
-        with an optional label, or a dictionary of observations.
-
-        Usage:
-        - add_obs(observation, name="some_label")
-        - add_obs({"label1": observation1, "label2": observation2})
+        Runs modrev with the given arguments
         """
+        try:
+            result = subprocess.run(
+                [self.modrev_path] + list(args),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True
+            )
+            return result
+        except Exception as e:
+            print(f"Error running modrev: {e}")
+            return None
 
-        # If the first argument is a dictionary, we assume it's a bulk addition of observations.
-        if len(args) == 1 and isinstance(args[0], dict):
-            for name, obs in args[0].items():
-                self._add_single_observation(obs, name)
-
-        # If there are two arguments, we assume it's a single observation with a label.
-        elif len(args) == 1 and 'name' in kwargs:
-            self._add_single_observation(args[0], kwargs['name'])
-
-        # If there's only one argument without a name, it's a single unnamed observation.
-        elif len(args) == 1:
-            self._add_single_observation(args[0], name=None)
-
-        # Otherwise, it's an error.
-        else:
-            raise ValueError("Invalid arguments for add_obs.")
-
-    def _add_single_observation(self, obs, name=None):
-        """
-        Private helper method to add a single observation to the model.
-        """
-        # Update dirty flag and observations dictionary
+    def add_obs(self, obs, name=None):
         self.dirty_flag = True
-        if name:
-            self.obs_dict[name] = obs
+        if isinstance(obs, dict):
+            self.observations.update(obs)
         else:
-            # Handle unnamed observation
-            self.obs_dict[f"observation_{len(self.obs_dict) + 1}"] = obs
+            self.observations[name if name else f"observation_{len(self.observations) + 1}"] = obs
 
     def is_consistent(self):
         """
         Checks if the current state of the model is consistent
         """
-        # saves lqm model onto file
-        # fixme: needs to take into account obs_dict
+        # todo: needs to take into account observations
         if self.dirty_flag:
-            self.modrev_file = save(self.lqm, self.modrev_file, "lp")
-            self.dirty_flag = False
-            
-        # runs modrev on files
-        try:
-            # Run modrev with the given model file to check consistency
-            result = subprocess.run(
-                [self.modrev_path, '-m', self.modrev_file, '-cc', '-v', '0'],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True
-            )
+            self._save_model_to_modrev_file()
 
-            if result.returncode != 0:
-                # There was an error running modrev
-                print(f"Error running modrev: {result}")
-                return False
+        result = self._run_modrev('-m', self.modrev_file, '-v', '0', '-cc')
 
-            # checks if output is consistent
-            # outpusts modrev answer
-            output = json.loads(result.stdout)
-            return output.get("consistent", False)
-
-        except Exception as e:
-            # Handle the case where modrev output is not valid JSON
-            print(f"Error parsing modrev output: {e}")
+        if not result or result.returncode != 0:
+            print(f"Error running modrev: {result}")
             return False
+
+        output = json.loads(result.stdout)
+        return output.get("consistent", False)
 
     def remove_obs(self, key):
         """
         Removes an observation from the dict by key
         """
-        self.obs_dict.pop(key)
+        self.observations.pop(key)
+        self.dirty_flag = True
 
     def set_obs(self, observations_dict):
         """
         Sets the observation dict
         """
-        self.obs_dict = observations_dict
+        self.observations = observations_dict
+        self.dirty_flag = True
 
     def stats(self):
         """
         Shows possible reparation actions in a friendly manner
         """
-        # implementation here
-        # save (lqm.to_modrev())
-        # run modrev on modrev file
-        # parse output
-        # return possible repairs
+        # todo: not taking into account observations yet
+        if self.dirty_flag:
+            self._save_model_to_modrev_file()
+
+        result = self._run_modrev('-m', self.modrev_file, '-v', '0')
+
+        if not result or result.returncode != 0:
+            print(f"Error running modrev: {result}")
+            return
+
+        # output of modrev comes in format:
+        # v1@F1,(v2) || (v3)
+        # change function to v1 = v2 || v3
+        output_lines = result.stdout.split("\n")
+        print("Possible repairs:")
+        for line in output_lines:
+            key = len(self.repairs.keys())
+            self.repairs[key] = line
+            print(f"Repair {key}: {line}")
+
 
     def generate_repairs(self, repair):
         """
         Generates a list of fixed lqm models (as JavaObjects)
         """
+        if not self.repairs[repair]:
+            print("Invalid repair")
+            return
+
         # clones the lqm model
-        # applies the repair directly on bioLQM
+        cloned_lqm = self.lqm.clone()
+
+        # todo: apply repair on cloned_lqm
         # returns all the generated models in the repair process
